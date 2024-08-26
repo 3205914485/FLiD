2fb24f210237ab24ee9ed1e5a85ce61f2b7b1a9f
zhk <1649466799@qq.com>
2024-08-26 23:12:24 +0800

    Add new file to gitignore

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 python train_ncem.py \
-    --prefix 0823_warmup_EM_1d_tgat_red_0.9_35\
-    --emodel_name TGAT\
+    --prefix 0826_EM_1d_tgn_red_0.8_02\
+    --emodel_name TGN\
+    --use_confidence 0\
+    --confidence_threshold 0.5\
     --decoder 1\
-    --start_runs 3\
-    --end_runs 5\
+    --start_runs 0\
+    --end_runs 2\
     --warmup_e_train 0\
     --warmup_m_train 0\
-    --gt_weight 0.9\
+    --gt_weight 0.8\
     --dataset_name reddit\
     --gpu 3\
 
